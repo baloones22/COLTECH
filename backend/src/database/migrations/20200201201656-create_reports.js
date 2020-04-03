6fbd512457e315f9cd8bf9d8b8202d22d8678e12
john <joaomenna1@gmail.com>
2020-04-03 19:31:43 -0400

    migrations reports change CASCADE

--- backend/src/database/migrations/20200201201656-create_reports.js
+++ backend/src/database/migrations/20200201201656-create_reports.js
@@ -11,7 +11,7 @@ module.exports = {
         type: Sequelize.INTEGER,
         references: { model: 'shop_keepers', key: 'id' },
         onUpdate: 'CASCADE',
-        onDelete: 'SET NULL',
+        onDelete: 'CASCADE',
         allowNull: true,
       },
       document_id: {
