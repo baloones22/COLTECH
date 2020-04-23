fc9d1a0069ff87162b98077d7ac3b245a2a7d1f8
Robert Garcia da Silva <rgs2@icomp.ufam.edu.br>
2020-04-23 11:51:06 -0400

    update variable of mail

--- backend/src/config/mail.js
+++ backend/src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: '2d8582fa00e729',
-    pass: '59f2ad04fda267',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe Brmalls <noreply@brmalls>',
