1dc5aa67abcc1ee47314476a7faabfd6e8d0b4ee
joao nogueira <joaomenna1@gmail.com>
2020-04-28 01:43:33 -0400

    new table files

--- backend/src/database/migrations/20200129142901-create_files.js
+++ backend/src/database/migrations/20200129142901-create_files.js
@@ -11,10 +11,18 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      path: {
+      size: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      key: {
         type: Sequelize.STRING,
         allowNull: false,
         unique: true,
+      },
+      url: {
+        type: Sequelize.STRING,
+        allowNull: false,
       },
       created_at: {
         type: Sequelize.DATE,
