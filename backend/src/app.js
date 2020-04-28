81a21cc2e9b044073a2a35eac6960d0cb558345e
joao nogueira <joaomenna1@gmail.com>
2020-04-28 01:44:59 -0400

    add urlencode in node js

--- backend/src/app.js
+++ backend/src/app.js
@@ -25,6 +25,7 @@ class App {
   middlewares() {
     // this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
+    this.server.use(express.urlencoded({ extended: true }));
     this.server.use(
       '/files',
       express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
